d95fe9dda2141c6ce853d72746a343e56ab162d1
tloszkov <kzsolt82@gmail.com>
2025-01-30 00:10:18 +0000

    fix: Simplify about page layout by removing unnecessary app structure

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -4,14 +4,8 @@
 
 <template>
   <v-responsive class="border rounded" max-height="300">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
-
           <h1>About</h1>
           <p>This is the about page.</p>
-      </v-app-bar>
-    </v-app>
   </v-responsive>
 </template>
 
