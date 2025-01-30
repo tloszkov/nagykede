dcd0f5cf35393bfe2296352b6395588f4bd4e1a3
tloszkov <kzsolt82@gmail.com>
2025-01-30 00:32:04 +0000

    feat: Add light and dark theme configurations to Vuetify plugin

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -15,5 +15,35 @@ import { createVuetify } from 'vuetify'
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+    themes: {
+      light: {
+        colors: {
+          background: '#e8dcac',
+          surface: 'rgba(152,129,98,0.86)',
+          primary: '#6200EE',
+          'primary-darken-1': '#3700B3',
+          secondary: '#03DAC6',
+          'secondary-darken-1': '#018786',
+          error: '#B00020',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        }
+      },
+      dark: {
+        colors: {
+          background: '#0d3b8e',
+          surface: '#0b104a',
+          primary: '#BB86FC',
+          'primary-darken-1': '#3700B3',
+          secondary: '#03DAC5',
+          'secondary-darken-1': '#03DAC5',
+          error: '#CF6679',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        }
+      }
+    }
   },
 })
