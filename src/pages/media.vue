69a3a075874cbda60d28ebaa782c434748b4c745
tloszkov <kzsolt82@gmail.com>
2025-01-30 23:05:00 +0000

    feat: Add MediaNavigationMenu component and update media page with new MediaGallery entries

--- src/pages/media.vue
+++ src/pages/media.vue
@@ -4,9 +4,17 @@
 
 <template>
   <h1 align="center">Média tár</h1>
-  <MediaGallery folderPath="muzeum"></MediaGallery>
-  <MediaGallery folderPath="2000"></MediaGallery>
-  <MediaGallery folderPath="2001"></MediaGallery>
+  <MediaGallery folderPath="2017 bal csoportkep"></MediaGallery>
+  <MediaGallery folderPath="2018 - Falunap"></MediaGallery>
+  <MediaGallery folderPath="2018 Arviz"></MediaGallery>
+  <MediaGallery folderPath="2018 Aszfalt"></MediaGallery>
+  <MediaGallery folderPath="2019-2021 kulturhaz kibovitese"></MediaGallery>
+  <MediaGallery folderPath="2019 bal csoportkep"></MediaGallery>
+  <MediaGallery folderPath="2019 Lakodalom"></MediaGallery>
+  <MediaGallery folderPath="2023 100eves a nagyharang"></MediaGallery>
+  <MediaGallery folderPath="epitett orokseg"></MediaGallery>
+  <MediaGallery folderPath="Muzeumtol"></MediaGallery>
+  <MediaGallery folderPath="Udvarhelyi konyvtar"></MediaGallery>
 
 </template>
 
